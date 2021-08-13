b0b33d01227432a562ec2cc8c5b730497df9b54a
Алексей <76786794+Sometich@users.noreply.github.com>
2021-08-13 23:54:14 +0300

    Update ReadabilityScoreType2.java

--- ReadabilityScoreType2.java
+++ ReadabilityScoreType2.java
@@ -6,7 +6,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+public class ReadabilityScoreType2 {
 
     public static void main(String[] args) {
 
